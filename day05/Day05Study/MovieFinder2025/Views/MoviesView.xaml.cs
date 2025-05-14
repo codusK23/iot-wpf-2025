32763e489db7cb6ace838a30140377ac5dfc3cb3
codusK23 <codusK23@gmail.com>
2025-05-14 17:23:53 +0900

    day05 commit

---
+++ day05/Day05Study/MovieFinder2025/Views/MoviesView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+using MovieFinder2025.Helpers;
+
+namespace MovieFinder2025.Views
+{
+    /// <summary>
+    /// MoviesView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MoviesView : MetroWindow
+    {
+        public MoviesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
